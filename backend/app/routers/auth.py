2e5fc1e50e2cb5454422ab5bb35417be82075942
Kushagra Jadiya <kjadiya2004@gmail.com>
2026-09-15 23:20:43 +0530

    feat: Authentication Login Working

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models.user import User
 from app.schemas.user import UserCreate, UserOut
-from app.auth import hash_password
+from fastapi.security import OAuth2PasswordRequestForm
+from app.auth import hash_password, verify_password, create_access_token
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -21,3 +22,13 @@ def register(user_in: UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@router.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == form_data.username).first()
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Incorrect email or password")
+
+    access_token = create_access_token(data={"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
