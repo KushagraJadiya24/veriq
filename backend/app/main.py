aacc5fb6c99c019739e1f76f9cbeb34640d251f8
Kushagra Jadiya <kjadiya2004@gmail.com>
2026-09-25 17:57:32 +0530

    CORS Enabled

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,8 +3,16 @@ from app.config import settings
 from app.database import Base, engine
 from app.models import user, workspace, db_connection, schema_embedding, audit_log
 from app.routers import auth, workspace, query
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth.router)
 app.include_router(workspace.router)
 app.include_router(query.router)
